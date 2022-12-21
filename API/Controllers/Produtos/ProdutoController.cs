4626a0a79216e66882b992ad1e4b57c85d2e07fc
iago-mota-dev <iagodeveloper144@gmail.com>
2022-12-21 09:59:22 -0300

    Create Api and DB

---
+++ API/Controllers/Produtos/ProdutoController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Aplicacao.Produtos.Servicos.Interfaces;
+using DataTransfer.Produtos.Requests;
+using DataTransfer.Produtos.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Produtos
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutosAppServico produtosAppServico;
+        public ProdutoController(IProdutosAppServico produtosAppServico)
+        {
+            this.produtosAppServico = produtosAppServico;
+        }
+        [HttpPost]
+        public ActionResult<ProdutoInserirResponse> Inserir([FromBody] ProdutoInserirRequest produtoRequest)
+        {
+            var produto = produtosAppServico.InserirProduto(produtoRequest);
+            return Ok(produto);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ProdutoResponse> Recuperar(int id)
+        {
+            var produto = produtosAppServico.Recuperar(id);
+            return Ok(produto);
+        }
+
+    }   
+}
---
+++ API/Program.cs
@@ -0,0 +1,89 @@
+using Aplicacao.Produtos.Profiles;
+using Aplicacao.Produtos.Servicos;
+using Dominio.Produtos.Servicos;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using Infra.Produtos;
+using Infra.Produtos.Mapeamentos;
+using NHibernate;
+using ISession = NHibernate.ISession;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(ProdutoProfile));
+
+builder.Services.AddSingleton<ISessionFactory>(factory =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("MySql");
+    return Fluently.Configure()
+    .Database((MySQLConfiguration.Standard.ConnectionString(connectionString)
+    .FormatSql()
+    .ShowSql()))
+    .Mappings(x =>
+    {
+
+        x.FluentMappings.AddFromAssemblyOf<ProdutosMap>();
+
+
+
+    }).BuildSessionFactory();
+});
+
+
+builder.Services.AddSingleton<ISession>(factory =>
+{
+    return factory.GetService<ISessionFactory>()!.OpenSession();
+});
+
+builder.Services.Scan(scan =>
+    scan.FromAssemblyOf<ProdutosAppServico>()
+    .AddClasses()
+    .AsImplementedInterfaces()
+    .WithSingletonLifetime()
+
+    .FromAssemblyOf<ProdutosServico>()
+    .AddClasses()
+    .AsImplementedInterfaces()
+    .WithSingletonLifetime()
+
+    .FromAssemblyOf<ProdutosRepositorio>()
+    .AddClasses()
+    .AsImplementedInterfaces()
+    .WithSingletonLifetime());
+
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    
+    app.UseSwaggerUI();
+
+    app.UseCors(c=>{
+        c.AllowAnyHeader();
+        c.AllowAnyMethod();
+        c.AllowAnyOrigin();
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
